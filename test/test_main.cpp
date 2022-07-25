646bc60c1ac20a221009edece2f1116778162c2e
Justin Nesselrotte <admin@jnesselr.org>
2022-07-25 17:13:56 -0600

    Added RS485 bus class

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -5,7 +5,7 @@
 #ifdef UNIT_TEST
 
 #include "test_assertable_buffer.h"
-//#include "test_rs485bus.h"
+#include "test_rs485bus.h"
 
 #define RUN_TEST_GROUP(TEST) \
     if (!std::getenv("TEST_GROUP") || (strcmp(#TEST, std::getenv("TEST_GROUP")) == 0)) { \
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
     UNITY_BEGIN();
 
     RUN_TEST_GROUP(AssertableBufferTest);
-    //RUN_TEST_GROUP(RS485BusTest);
+    RUN_TEST_GROUP(RS485BusTest);
 
     UNITY_END();
 
